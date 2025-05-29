a99a57703698c7507a8c497e9ea48b671ab7c328
tumblingfyrfox <tumblingfyrfox@gmail.com>
2025-05-29 21:49:33 +0530

    ✨ Updated: 2025-05-29 21:49:33 — {{file}}
    Affected files:
    docs/03_js-potions/page/closures/closures.js

--- docs/03_js-potions/page/closures/closures.js
+++ docs/03_js-potions/page/closures/closures.js
@@ -7,3 +7,30 @@ function outerFn(){
 }
 
 console.log("Basic Closure : ", outerFn()())
+
+for(var i=0;i< 2;i++){
+    console.log("inside loop", i,outerFn()());
+}
+
+function returnsParam(n){
+    
+    return function() {
+       return n;
+    }
+}
+
+for(var i=0;i< 2;i++){
+    console.log("inside loop, returns param", i,returnsParam(i)());
+}
+
+for(var i=0;i< 2;i++){
+    setTimeout(function(){
+    console.log("inside loop, returns param, call inside setTimeout", i,returnsParam(i)());
+})
+}
+
+for(let i=0;i< 2;i++){
+    setTimeout(function(){
+    console.log("using let inside loop, returns param, call inside setTimeout", i,returnsParam(i)());
+})
+}
