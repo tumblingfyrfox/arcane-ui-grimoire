cca6257b3b34c3d14ece955264c1e3a72def8899
tumblingfyrfox <tumblingfyrfox@gmail.com>
2025-05-29 22:16:07 +0530

    ✨ Updated: 2025-05-29 22:16:07 — {{file}}
    Affected files:
    docs/03_js-potions/page/closures/closures.js

--- docs/03_js-potions/page/closures/closures.js
+++ docs/03_js-potions/page/closures/closures.js
@@ -34,3 +34,37 @@ for(let i=0;i< 2;i++){
     console.log("using let inside loop, returns param, call inside setTimeout", i,returnsParam(i)());
 })
 }
+
+
+
+
+
+
+function addNum1(){
+    var a = 10;
+    var b =15;
+    function sum() {
+        return a+b;
+    }
+    return sum;
+}
+
+
+
+var  x1 = addNum1();
+console.log(x1())
+
+
+function addNum(){
+    var a = 10;
+    var b =15;
+    return sum;
+}
+
+function sum() {
+    return a+b;
+}
+
+
+var  x = addNum();
+console.log(x())
